6a89ffb17b044d9af4885d6fbe7c9285392439b3
Ging1991 <carloscaballeromorel@gmail.com>
2018-09-01 19:43:34 -0300

    Version final de la agenda solo falta el instalador

--- src/main/java/persistencia/Conexion.java
+++ src/main/java/persistencia/Conexion.java
@@ -8,12 +8,17 @@ import org.apache.log4j.Logger;
 public class Conexion {
 	public static Conexion instancia;
 	private Connection connection;
-	private Logger log = Logger.getLogger(Conexion.class);	
+	private Logger log = Logger.getLogger(Conexion.class);
 	
 	private Conexion() {
 		try	{
+			String IP = Propiedades.recuperar("IP");
+			String puerto = Propiedades.recuperar("puerto");
+			String usuario = Propiedades.recuperar("usuario");
+			String password = Propiedades.recuperar("password");
+			
 			Class.forName("com.mysql.jdbc.Driver");
-			this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda","root","root");
+			this.connection = DriverManager.getConnection("jdbc:mysql://"+IP+":"+puerto+"/agenda", usuario, password);
 			log.info("Conexión exitosa");
 		}
 		
